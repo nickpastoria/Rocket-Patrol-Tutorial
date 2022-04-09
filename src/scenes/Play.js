c75c64ecd6678de2261d776f45e9d13fa72f61fd
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-04-09 14:15:24 -0700

    Creation of basic spaceship

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,11 +26,16 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
-
+        this.ship01.update();               // update spaceships (x3)
+        this.ship02.update();
+        this.ship03.update();
         this.p1Rocket.update();
     }
 }
